e15c3f4b220fad0a85d7b95b10b873050c49f5dc
michelle zhang <miczhang@umich.edu>
2019-08-20 16:32:24 -0400

    missing js files

---
+++ js/navigation_bar.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const e = React.createElement;
+
+class NavigationBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selected: ""};
+    }
+
+    render(){
+        return e(
+            
+        );
+    }
+}
